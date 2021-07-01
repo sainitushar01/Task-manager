fb347f58bd81622ba160f59ec092806491258636
Tushar Saini <60431836+sainitushar01@users.noreply.github.com>
2021-07-01 11:37:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -171,4 +171,9 @@ const saveEditchanges = (event) => {
   });
 
   updateLocalStorage();
+  taskTitle.setAttribute("contenteditable","false");
+  taskDescription.setAttribute("contenteditable", "false");
+  taskType.setAttribute("contenteditable", "false");
+submitButton.removeAttribute("onclick");
+  submitButton.innerHTML="Open Task";
 };
